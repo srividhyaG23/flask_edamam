14653dbd457f0c4118478fc26d6c2f0ac9504511
James <IdiosApps@users.noreply.github.com>
2020-07-25 10:43:16 +0100

    Use Flask to create /recipes endpoint, and grab the "ingredient" query param so we can return relevant recipes from Edamam

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import os
 import requests
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/')
+def home():
+    return "hello world"
+
+
+@app.route('/recipes')
+def search():
+    ingredient = request.args.get('ingredient')
+
+    response = edamam_search(ingredient)
+    return response
 
 
 # Following example usage from https://developer.edamam.com/edamam-docs-recipe-api
@@ -9,20 +27,24 @@ def edamam_search(query):
     app_id = os.environ['APP_ID']
     app_key = os.environ['APP_KEY']
 
-    request = f"https://api.edamam.com/search?q={query}" \
-              f"&app_id={app_id}" \
-              f"&app_key={app_key}"
+    curl = f"https://api.edamam.com/search?q={query}" \
+           f"&app_id={app_id}" \
+           f"&app_key={app_key}"
 
-    response = requests.get(request)
+    response = requests.get(curl)
     hits = response.json()['hits']
 
+    response = ""
     for hit in hits:
         recipe = hit['recipe']
-        print(f"---{recipe['label']}---")
+        response += f"---{recipe['label']}---<br>"
         for ingredient in recipe['ingredients']:
-            print(f"   {ingredient['text']}")
+            response += f"{ingredient['text']}<br>"
+
+    return response
 
 
 if __name__ == '__main__':
-    edamam_search('nutritional yeast')
+    app.run()
 
+    # edamam_search('nutritional yeast')
